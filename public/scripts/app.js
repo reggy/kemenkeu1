aed2c07893379ae8b9ac35436e53f1dc71062a89
Regi Ruhyana <reggy@mediawave.co.id>
2015-10-06 14:46:47 +0700

    tambahkan statis file css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,67 @@
-// function 
+
+var apiUrl_2 = 'http://localhost:5300/';
+
+var $urlTrend = apiUrl_2 + 'api/linechart';
+var trend = $.ajax({type: "GET", url: $urlTrend, async: false}).responseText;
+
+var $trendchart = JSON.parse(trend);
+var $result = [],$cat = [],$datas = [];
+var priceTrend = $trendchart[0].data;
+var arrTrend = [];
+var dates = $trendchart[0].date;
+if(priceTrend.length > 0){
+	for(var i =0; i < priceTrend.length; i++){
+		// 
+		// var spliter = dates.split("-");
+		// var month = spliter[1];
+		// var day = spliter[2];
+		// arrTrend.push([month+"-"+day]);
+		// $datas.push(parseFloat(priceTrend[i].price));
+
+	}
+	// console.log("dates : ",dates);
+}
+
+
+$result = [{data: $datas}];
+jQuery('#linechart').highcharts({
+	chart: {
+		defaultSeriesType: 'line',
+		backgroundColor:'rgba(255, 255, 255, 0.1)'
+	},
+	tooltip: {
+		formatter: function() {
+			return '<b>' + this.x + '</b><br/>' +
+			' Total ' + Highcharts.numberFormat(this.y, 0);
+		}
+	},
+	title: {
+		text: 'Trend Harga',
+		style: {
+			fontFamily: 'sans-serif',
+			color: "#000"
+		}
+	},
+	exporting: {enabled: false},
+	credits: {enabled: false},
+	legend: {enabled: false},
+	xAxis: {
+		categories: dates
+	},
+	yAxis: {
+		min: 1,
+		title: {
+			text: '',
+		},
+		labels: {
+			overflow: 'justify',
+			style: {
+				color: '#000',
+				font: '11px Trebuchet MS, Verdana, sans-serif'
+			}
+		}
+	},
+	series: $result
+});
+
+/**/
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.engine('html',require('ejs').renderFile);
 app.set('view engine','html');
 
 app.use('/css', express.static(__dirname + '/public/styles'));
+app.use('/scripts', express.static(__dirname + '/public/scripts'));
 
 app.get('/',function(request,response){
 	// response.send('halo dunia aja');
@@ -50,8 +51,25 @@ app.get('/api/linechart',function(req,res){
 		data : [15,3,40.2,90,120]
 	}];
 
-	// var $datas = JSON.stringify($data);
-	var mydatas = [{date:$catLinechart,data : $dataLineChart}];
+	var mydatas = JSON.stringify([{date:$catLinechart,data : $dataLineChart}]);
+	res.writeHead(200,{"Content-type" : "application/json"});
+	res.end(mydatas);
+});
+
+app.get('/api/barchart',function(req,res){
+	var $catBar = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+	var $dataBar = [{
+		name: 'Year 1800',
+		data: [107, 31, 635, 203, 2]
+	}, {
+		name: 'Year 1900',
+		data: [133, 156, 947, 408, 6]
+	}, {
+		name: 'Year 2012',
+		data: [1052, 954, 4250, 740, 38]
+	}];
+
+	var mydatas = JSON.stringify([{date:$catBar,data : $dataBar}]);
 	res.writeHead(200,{"Content-type" : "application/json"});
 	res.end(mydatas);
 });
